cca2ba7fd9281b8b991dc46153e16a0e825ff987
Oleksii Zhyvotivskyi <gamesdashev@gmail.com>
2024-08-16 14:17:33 +0200

    Добавление файлов для GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/my-app/',  // Замените "my-app" на имя вашего репозитория
+  base: '/my-app/',  
   plugins: [react()],
 })
