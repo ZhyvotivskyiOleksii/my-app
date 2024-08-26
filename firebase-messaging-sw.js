2740342f621fed9ead0a183d35a9b1b4126095d6
Oleksii Zhyvotivskyi <gamesdashev@gmail.com>
2024-08-26 09:24:25 +0200

    Updates

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,27 @@
+
+importScripts('https://www.gstatic.com/firebasejs/9.1.3/firebase-messaging.js');
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAgkl94QmYAAfz4xGRdyQNCWMOBU5ZYhQk",
+  authDomain: "appsuperbet-33e39.firebaseapp.com",
+  projectId: "appsuperbet-33e39",
+  storageBucket: "appsuperbet-33e39.appspot.com",
+  messagingSenderId: "313043480351",
+  appId: "1:313043480351:web:79f63add28ae0ccc984a2a",
+  measurementId: "G-JZKH24GDLT"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/favicon.ico'
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
