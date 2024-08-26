4d77e6b2de73299a87ea5f8ba61449226743a7ec
Oleksii Zhyvotivskyi <gamesdashev@gmail.com>
2024-08-26 09:20:04 +0200

    Deploy to GitHub Pages

--- src/firebase.js
+++ src/firebase.js
@@ -16,7 +16,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Auth and Firestore
+
 const auth = getAuth(app);
 const firestore = getFirestore(app);
 
